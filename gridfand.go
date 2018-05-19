7fb0eabbc63b5953edfc3372ab4e7956b7ce2bf1
Jan Kasiak <jan@cybojanek.net>
2018-05-19 12:29:17 -0400

    Lint code

--- gridfand.go
+++ gridfand.go
@@ -3,8 +3,8 @@ package main
 import (
     "bytes"
     "encoding/json"
-    "errors"
     "fmt"
+    "github.com/tarm/serial"
     "io/ioutil"
     "log"
     "os"
@@ -12,62 +12,66 @@ import (
     "strconv"
     "strings"
     "time"
-    "github.com/tarm/serial"
 )
 
-const MinFanIndex = 1
-const MaxFanIndex = 6
-const MinFanRPM = 20
-const MaxFanRPM = 100
-
+// Controller minimums and maximums
 const (
-    DISK_STATUS_ASLEEP = iota
-    DISK_STATUS_STANDBY
-    DISK_STATUS_ACTIVE
+    MinFanIndex = 1
+    MaxFanIndex = 6
+    MinFanRPM   = 20
+    MaxFanRPM   = 100
 )
 
+// Disk status
+const (
+    DiskStatusSleep = iota
+    DiskStatusStandby
+    DiskStatusActive
+)
+
+// DiskGroup of disks
 type DiskGroup struct {
     Disks []string
 }
 
+// FanController for GridFan
 type FanController struct {
     DevicePath string
 
     serial *serial.Port
 }
 
+// Config for GridFan
 type Config struct {
-    DevicePath string       `json:"serial_device_path"`
-    ConstantRPM map[int]int `json:"constant_rpm"`
+    DevicePath     string      `json:"serial_device_path"`
+    ConstantRPM    map[int]int `json:"constant_rpm"`
     DiskControlled struct {
-        Fans []int            `json:"fans"`
-        TargetTemperature int `json:"target_temp"`
-        SleepingTimeout int   `json:"sleeping_timeout"`
-        Disks []string        `json:"disks"`
-        RPM struct {
-            Sleeping int    `json:"sleeping"`
-            Standby int     `json:"standby"`
+        Fans              []int    `json:"fans"`
+        TargetTemperature int      `json:"target_temp"`
+        SleepingTimeout   int      `json:"sleeping_timeout"`
+        Disks             []string `json:"disks"`
+        RPM               struct {
+            Sleeping int `json:"sleeping"`
+            Standby  int `json:"standby"`
         } `json:"rpm"`
     } `json:"disk_controlled"`
 }
 
-func min(a int, b int) (int) {
+func min(a int, b int) int {
     if a < b {
         return a
-    } else {
-        return b
     }
+    return b
 }
 
-func max(a int, b int) (int) {
+func max(a int, b int) int {
     if a > b {
         return a
-    } else {
-        return b
     }
+    return b
 }
 
-// Get the maximum disk temperature of a list of disk
+// GetTemperature maximum of all disks
 func (diskGroup DiskGroup) GetTemperature() (int, error) {
     maxTemperature := 0
     for _, disk := range diskGroup.Disks {
@@ -90,13 +94,14 @@ func (diskGroup DiskGroup) GetTemperature() (int, error) {
         // Split into lines
         lines := strings.Split(strings.TrimSpace(stringOutput), "\n")
         if len(lines) != 1 {
-            return maxTemperature, errors.New(fmt.Sprintf("output is not one line: %v",
-                stringOutput))
+            return maxTemperature, fmt.Errorf(
+                "GetTemperature: output is not one line: %v", stringOutput)
         }
 
         // Check for error, since hddtemp returns exit cide 0
         if strings.Contains(lines[0], "No such file or directory") {
-            return maxTemperature, errors.New(fmt.Sprintf("Disk [%v] not found", disk))
+            return maxTemperature, fmt.Errorf(
+                "GetTemperature: Disk [%v] not found", disk)
         }
 
         // Check if drive is asleep
@@ -107,8 +112,8 @@ func (diskGroup DiskGroup) GetTemperature() (int, error) {
         // Get temperature
         fields := strings.Split(lines[0], ":")
         if len(fields) != 3 {
-            return maxTemperature, errors.New(fmt.Sprintf("output is not three fields: %v",
-                lines[0]))
+            return maxTemperature, fmt.Errorf(
+                "GetTemperature: output is not three fields: %v", lines[0])
         }
 
         field := strings.TrimSpace(fields[2])
@@ -117,21 +122,21 @@ func (diskGroup DiskGroup) GetTemperature() (int, error) {
             if c < '0' || c > '9' {
                 break
             }
-            tempStr = field[0:i + 1]
+            tempStr = field[0 : i+1]
         }
 
-        if temperature, err := strconv.Atoi(tempStr); err != nil {
+        temperature, err := strconv.Atoi(tempStr)
+        if err != nil {
             return maxTemperature, err
-        } else  {
-            maxTemperature = max(temperature, maxTemperature)
         }
+        maxTemperature = max(temperature, maxTemperature)
     }
     return maxTemperature, nil
 }
 
-// Get the highest activity status of a list of disks
+// GetStatus of highest activity disk
 func (diskGroup DiskGroup) GetStatus() (int, error) {
-    status := DISK_STATUS_ASLEEP
+    status := DiskStatusSleep
     for _, disk := range diskGroup.Disks {
         // Get command output
         command := exec.Command("hdparm", "-C", disk)
@@ -151,67 +156,68 @@ func (diskGroup DiskGroup) GetStatus() (int, error) {
         // Split into lines
         lines := strings.Split(strings.TrimSpace(stringOutput), "\n")
         if len(lines) != 2 {
-            return status, errors.New(fmt.Sprintf("output is not two lines: %v",
-                stringOutput))
+            return status, fmt.Errorf(
+                "GetStatus: output is not two lines: %v", stringOutput)
         }
 
         // NOTE: our notion of standby differs from what hdparm reports...
-        status_line := lines[1]
+        statusLine := lines[1]
         switch {
-        case strings.Contains(status_line, "standby"):
-            status = max(status, DISK_STATUS_ASLEEP)
+        case strings.Contains(statusLine, "standby"):
+            status = max(status, DiskStatusSleep)
 
-        case strings.Contains(status_line, "unknown"):
-            status = max(status, DISK_STATUS_STANDBY)
+        case strings.Contains(statusLine, "unknown"):
+            status = max(status, DiskStatusStandby)
 
-        case strings.Contains(status_line, "active/idle"):
-            status = max(status, DISK_STATUS_ACTIVE)
+        case strings.Contains(statusLine, "active/idle"):
+            status = max(status, DiskStatusActive)
 
         default:
-            return status, errors.New(fmt.Sprintf("bad status line: [%s]", status_line))
+            return status, fmt.Errorf(
+                "GetStatus: bad status line: [%s]", statusLine)
         }
     }
     return status, nil
 }
 
 // Write all bytes to the serial device
-func (controller *FanController) writeFully(b []byte) (error) {
+func (controller *FanController) writeFully(b []byte) error {
     written := 0
     for written < len(b) {
-        if n, err := controller.serial.Write(b[written:]); err != nil {
+        n, err := controller.serial.Write(b[written:])
+        if err != nil {
             return err
-        } else {
-            written += n
         }
+        written += n
     }
     return nil
 }
 
 // Read until array is full
-func (controller *FanController) readFully(b []byte) (error) {
+func (controller *FanController) readFully(b []byte) error {
     read := 0
     for read < len(b) {
-        if n, err := controller.serial.Read(b[read:]); err != nil {
+        n, err := controller.serial.Read(b[read:])
+        if err != nil {
             return err
-        } else {
-            read += n
         }
+        read += n
     }
     return nil
 }
 
 // Open controller
-func (controller *FanController) Open() (error) {
+func (controller *FanController) Open() error {
     if controller.serial != nil {
         return nil
     }
 
     c := &serial.Config{Name: controller.DevicePath, Baud: 4800}
-    if s, err := serial.OpenPort(c); err != nil {
+    s, err := serial.OpenPort(c)
+    if err != nil {
         return err
-    } else {
-        controller.serial = s
     }
+    controller.serial = s
 
     controller.serial.Flush()
 
@@ -219,14 +225,14 @@ func (controller *FanController) Open() (error) {
     if err := controller.Ping(); err != nil {
         // Close, we already have an error...so ignore Close error
         controller.Close()
-        return errors.New(fmt.Sprintf("Failed to ping controller: %v\n", err))
+        return fmt.Errorf("Open: Failed to ping controller: %v", err)
     }
 
     return nil
 }
 
 // Close controller
-func (controller *FanController) Close() (error) {
+func (controller *FanController) Close() error {
     if controller.serial == nil {
         return nil
     }
@@ -241,9 +247,9 @@ func (controller *FanController) Close() (error) {
 }
 
 // Ping controller to check its alive
-func (controller *FanController) Ping() (error) {
+func (controller *FanController) Ping() error {
     if controller.serial == nil {
-        return errors.New(fmt.Sprintf("Controller is not open"))
+        return fmt.Errorf("Ping: Controller is not open")
     }
 
     data := []byte{0xc0}
@@ -257,21 +263,21 @@ func (controller *FanController) Ping() (error) {
     }
 
     if reply[0] != 0x21 {
-        return errors.New(fmt.Sprintf("Unexpected reply: %d", reply[0]))
+        return fmt.Errorf("Ping: Unexpected reply: %d", reply[0])
     }
 
     return nil
 }
 
-// Get the speed of a fan
+// GetSpeed of a fan
 func (controller *FanController) GetSpeed(fan int) (int, error) {
     speed := 0
     if controller.serial == nil {
-        return speed, errors.New(fmt.Sprintf("Controller is not open"))
+        return speed, fmt.Errorf("GetSpeed: Controller is not open")
     }
 
     if !isValidFan(fan) {
-        return speed, errors.New(fmt.Sprintf("Bad fan number: %d", fan))
+        return speed, fmt.Errorf("GetSpeed: Bad fan number: %d", fan)
     }
 
     data := []byte{0x8a, byte(fan)}
@@ -285,7 +291,7 @@ func (controller *FanController) GetSpeed(fan int) (int, error) {
     }
 
     if !bytes.Equal(reply[0:3], []byte{0xc0, 0x00, 0x00}) {
-        return speed, errors.New(fmt.Sprintf("Malformed reply: %v", reply))
+        return speed, fmt.Errorf("GetSpeed: Malformed reply: %v", reply)
     }
 
     speed = (int(reply[3]) << 8) | int(reply[4])
@@ -293,18 +299,18 @@ func (controller *FanController) GetSpeed(fan int) (int, error) {
     return speed, nil
 }
 
-// Set the speed of a fan
-func (controller *FanController) SetSpeed(fan int, rpm int) (error) {
+// SetSpeed of a fan
+func (controller *FanController) SetSpeed(fan int, rpm int) error {
     if controller.serial == nil {
-        return errors.New(fmt.Sprintf("Controller is not open"))
+        return fmt.Errorf("SetSpeed: Controller is not open")
     }
 
     if !isValidFan(fan) {
-        return errors.New(fmt.Sprintf("Bad fan number: %d", fan))
+        return fmt.Errorf("SetSpeed: Bad fan number: %d", fan)
     }
 
     if !isValidRPM(rpm) {
-        return errors.New(fmt.Sprintf("Bad fan rpm: %d", rpm))
+        return fmt.Errorf("SetSpeed: Bad fan rpm: %d", rpm)
     }
 
     byteA := byte(0)
@@ -325,19 +331,19 @@ func (controller *FanController) SetSpeed(fan int, rpm int) (error) {
     }
 
     if reply[0] != 0x1 {
-        return errors.New(fmt.Sprintf("Unexpected reply: %d", reply[0]))
+        return fmt.Errorf("SetSpeed: Unexpected reply: %d", reply[0])
     }
 
     return nil
 }
 
 // Check if a fan number is valid
-func isValidFan(fan int) (bool) {
+func isValidFan(fan int) bool {
     return fan >= MinFanIndex && fan <= MaxFanIndex
 }
 
 // Check if a fan RPM is valid
-func isValidRPM(rpm int) (bool) {
+func isValidRPM(rpm int) bool {
     return rpm == 0 || (rpm >= MinFanRPM && rpm <= MaxFanRPM)
 }
 
@@ -359,61 +365,65 @@ func readConfig(path string) (Config, error) {
 
     // Check DevicePath
     if len(config.DevicePath) == 0 {
-        return config, errors.New(fmt.Sprintf("Missing serial_device_path"))
+        return config, fmt.Errorf("readConfig: Missing serial_device_path")
     }
 
     // Check ConstantRPM fans
     for fan, rpm := range config.ConstantRPM {
         if !isValidFan(fan) {
-            return config, errors.New(fmt.Sprintf("Invalid fan index: %d", fan))
+            return config, fmt.Errorf("readConfig: Invalid fan index: %d", fan)
         }
         if !isValidRPM(rpm) {
-            return config, errors.New(fmt.Sprintf("Invalid fan %d rpm: %d", fan, rpm))
+            return config, fmt.Errorf(
+                "readConfig: Invalid fan %d rpm: %d", fan, rpm)
         }
     }
 
     // Check DiskControlled.Fans
     for _, fan := range config.DiskControlled.Fans {
         if !isValidFan(fan) {
-            return config, errors.New(fmt.Sprintf("Invalid fan index: %d", fan))
+            return config, fmt.Errorf("readConfig: Invalid fan index: %d", fan)
         }
 
         // Can only be present in one
         _, ok := config.ConstantRPM[fan]
         if ok {
-            return config, errors.New(fmt.Sprintf(
-                "Fan %d present in both constant_rpm and disk_controlled", fan))
+            return config, fmt.Errorf(
+                "readConfig: Fan %d present in both constant_rpm and disk_controlled", fan)
         }
     }
 
     // Check Sleeping and Standby
     if !isValidRPM(config.DiskControlled.RPM.Sleeping) {
-            return config, errors.New(fmt.Sprintf("Invalid sleeping rpm: %d",
-                config.DiskControlled.RPM.Sleeping))
+        return config, fmt.Errorf("readConfig: Invalid sleeping rpm: %d",
+            config.DiskControlled.RPM.Sleeping)
     }
 
     if !isValidRPM(config.DiskControlled.RPM.Standby) {
-            return config, errors.New(fmt.Sprintf("Invalid standby rpm: %d",
-                config.DiskControlled.RPM.Standby))
+        return config, fmt.Errorf("readConfig: Invalid standby rpm: %d",
+            config.DiskControlled.RPM.Standby)
     }
 
     // Check SleepingTimeout
     if config.DiskControlled.SleepingTimeout < 0 ||
-            config.DiskControlled.SleepingTimeout > 3600 {
-        return config, errors.New(fmt.Sprintf("Invalid sleeping_timeout: %d not in [0, 3600]",
-            config.DiskControlled.SleepingTimeout))
+        config.DiskControlled.SleepingTimeout > 3600 {
+        return config, fmt.Errorf(
+            "readConfig: Invalid sleeping_timeout: %d not in [0, 3600]",
+            config.DiskControlled.SleepingTimeout)
     }
 
     // Check TargetTemperature
     if config.DiskControlled.TargetTemperature < 0 ||
-            config.DiskControlled.TargetTemperature > 100 {
-        return config, errors.New(fmt.Sprintf("Invalid target_temp: %d not in [0, 100]",
-            config.DiskControlled.TargetTemperature))
+        config.DiskControlled.TargetTemperature > 100 {
+        return config, fmt.Errorf(
+            "readConfig: Invalid target_temp: %d not in [0, 100]",
+            config.DiskControlled.TargetTemperature)
     }
 
     return config, nil
 }
 
+// PID loop information
 // https://en.wikipedia.org/wiki/PID_controller#Pseudocode
 type PID struct {
     SetPoint float64
@@ -423,17 +433,19 @@ type PID struct {
     KD float64
 
     previousError float64
-    integral float64
-    previousTime time.Time
+    integral      float64
+    previousTime  time.Time
 }
 
+// Reset PID information
 func (pid *PID) Reset() {
     pid.previousError = 0
     pid.integral = 0
     pid.previousTime = time.Now()
 }
 
-func (pid *PID) UpdatePID(value float64) (float64) {
+// Update pid information
+func (pid *PID) Update(value float64) float64 {
     timeSince := time.Since(pid.previousTime).Seconds()
 
     error := pid.SetPoint - value
@@ -454,7 +466,7 @@ func monitorTemperature(config Config) {
     // Default is asleep in case of service restart. This means that if the
     // cooldown did not finish, then the cooldown will be shortened, but we
     // want that to avoid fan spinup on service restart.
-    lastStatus := DISK_STATUS_ASLEEP
+    lastStatus := DiskStatusSleep
     deadlineOff := time.Now()
     lastRPM := -1
 
@@ -462,7 +474,7 @@ func monitorTemperature(config Config) {
     // FIXME: tune parameters
     pid := PID{
         SetPoint: float64(config.DiskControlled.TargetTemperature),
-        KP: 3, KI: 5, KD: 3,
+        KP:       3, KI: 5, KD: 3,
     }
 
     for {
@@ -478,9 +490,9 @@ func monitorTemperature(config Config) {
         } else {
             switch status {
 
-            case DISK_STATUS_ASLEEP:
+            case DiskStatusSleep:
                 // Disks are turned off - turn off fans after a cooldown period
-                if lastStatus == DISK_STATUS_ASLEEP {
+                if lastStatus == DiskStatusSleep {
                     timeSince := time.Since(deadlineOff).Seconds()
                     if timeSince >= 0 {
                         targetRPM = 0
@@ -500,18 +512,18 @@ func monitorTemperature(config Config) {
                         deadlineOff, targetRPM)
                 }
 
-            case DISK_STATUS_STANDBY:
+            case DiskStatusStandby:
                 // Disks are neither fully turned off, and neither active
                 // Can't read temperature in this state
                 targetRPM = config.DiskControlled.RPM.Standby
                 log.Printf("INFO Disk status is standby, setting RPM to: %d", targetRPM)
 
-            case DISK_STATUS_ACTIVE:
+            case DiskStatusActive:
                 // Disks are active - check temperature
-                if lastStatus != DISK_STATUS_ACTIVE {
+                if lastStatus != DiskStatusActive {
                     pid.Reset()
                 } else {
-                    pid.UpdatePID(float64(temp))
+                    pid.Update(float64(temp))
                 }
 
                 // FIXME: finish implementing this...
@@ -582,9 +594,9 @@ func mainWrapper() (ret int) {
     ret = 1
 
     // Check usage
-    if  !((len(os.Args) == 3 && os.Args[2] == "daemon") ||
-            (len(os.Args) == 4 && os.Args[2] == "get") ||
-            (len(os.Args) == 5 && os.Args[2] == "set")) {
+    if !((len(os.Args) == 3 && os.Args[2] == "daemon") ||
+        (len(os.Args) == 4 && os.Args[2] == "get") ||
+        (len(os.Args) == 5 && os.Args[2] == "set")) {
         fmt.Fprintf(os.Stderr, "Usage: %v\n", os.Args[0])
         fmt.Fprintf(os.Stderr, "  JSON_CONFIG_FILE daemon\n")
         fmt.Fprintf(os.Stderr, "  JSON_CONFIG_FILE get all|1|2|3|4|5|6\n")
@@ -612,23 +624,23 @@ func mainWrapper() (ret int) {
         fans := []int{1, 2, 3, 4, 5, 6}
         if os.Args[3] != "all" {
             fans = fans[0:0]
-            if value, err := strconv.Atoi(os.Args[3]); err != nil || !isValidFan(value) {
+            value, err := strconv.Atoi(os.Args[3])
+            if err != nil || !isValidFan(value) {
                 fmt.Fprintf(os.Stderr, "Bad fan index: %v\n", os.Args[3])
                 return
-            } else {
-                fans = append(fans, value)
             }
+            fans = append(fans, value)
         }
 
         // Parse rpm
         rpm := 0
         if os.Args[2] == "set" {
-            if value, err := strconv.Atoi(os.Args[4]); err != nil || !isValidRPM(value) {
+            value, err := strconv.Atoi(os.Args[4])
+            if err != nil || !isValidRPM(value) {
                 fmt.Fprintf(os.Stderr, "Bad fan RPM: %v\n", os.Args[4])
                 return
-            } else {
-                rpm = value
             }
+            rpm = value
         }
 
         // Open controller
@@ -649,12 +661,12 @@ func mainWrapper() (ret int) {
         // Run command
         for _, fan := range fans {
             if os.Args[2] == "get" {
-                if rpm, err := controller.GetSpeed(fan); err != nil {
+                rpm, err := controller.GetSpeed(fan)
+                if err != nil {
                     fmt.Fprintf(os.Stderr, "Failed to get speed: %v\n", err)
                     return
-                } else {
-                    fmt.Printf("%d %d\n", fan, rpm)
                 }
+                fmt.Printf("%d %d\n", fan, rpm)
             } else {
                 if err := controller.SetSpeed(fan, rpm); err != nil {
                     fmt.Fprintf(os.Stderr, "Failed to set speed: %d %d %v\n",
